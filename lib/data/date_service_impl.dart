56d162ddbfa269f2e7ec26f89e21b1a6de218802
AnthonyMde <anthony.mamode@gmail.com>
2024-11-10 19:59:44 +0100

    Add current date + number of past days.

--- lib/data/date_service_impl.dart
+++ lib/data/date_service_impl.dart
@@ -3,4 +3,11 @@ import 'package:challenge_365d/domain/date_service.dart';
 class DateServiceImpl implements DateService {
   @override
   DateTime getCurrentUTCDate() => DateTime.now();
+
+  @override
+  int getPastChallengeDays() {
+    final start = DateTime(2024, 11, 05);
+    final now = DateTime.now();
+    return now.difference(start).inDays;
+  }
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,3 @@
-import 'package:challenge_365d/providers/counter_provider.dart';
 import 'package:challenge_365d/providers/date_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -11,44 +10,41 @@ class HomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final counter = ref.watch(counterProvider);
     final dateService = ref.watch(dateServiceProvider);
 
     final now = dateService.getCurrentUTCDate();
     final formattedDate = DateFormat("EEE, dd MMMM yyyy", "en_US").format(now);
+    final pastDays = dateService.getPastChallengeDays();
 
     return Scaffold(
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.only(
-            left: 16.0,
-            top: 16.0,
-            right: 16.0,
-            bottom: 16.0
-          ),
+              left: 16.0, top: 16.0, right: 16.0, bottom: 16.0),
           child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.start,
               children: <Widget>[
                 const SizedBox(height: 16.0),
-                Text(formattedDate),
-                const Text(
-                  'You have pushed the button this many times:',
+                Text(
+                  formattedDate,
+                  style: const TextStyle(
+                      fontSize: 20,
+                      fontStyle: FontStyle.italic,
+                      color: Colors.black87),
                 ),
                 Text(
-                  '$counter',
-                  style: Theme.of(context).textTheme.headlineMedium,
+                  "${pastDays.toString()} days",
+                  style: const TextStyle(
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.deepOrangeAccent),
                 ),
               ],
             ),
           ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => ref.read(counterProvider.notifier).increment(),
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
