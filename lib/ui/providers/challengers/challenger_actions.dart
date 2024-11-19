90d315d5f2ad4bfb7c8e9deb8104c48c5b223f21
AnthonyMde <anthony.mamode@gmail.com>
2024-11-19 23:04:09 +0100

    User can only modify its own challenges.

--- lib/ui/providers/challengers/challenger_actions.dart
+++ lib/ui/providers/challengers/challenger_actions.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kaizen/domain/usecases/providers/get_account_use_case_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 import '../../../domain/models/challenger.dart';
@@ -18,6 +19,12 @@ class ChallengerActions {
 
   Future<void> toggleChallengeState(
       String challengerId, Challenge challenge) async {
+    final account = await ref.read(getAccountUseCaseProvider).call();
+    // TODO show toast
+    // TODO move to use case?
+    if (account == null || account.id != challengerId) {
+      return;
+    }
     await ref
         .read(challengerRepositoryProvider)
         .toggleChallengeState(challengerId, challenge);
