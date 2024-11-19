0449f98bbea4e813797ac7e9d8483ad5a29fed6a
AnthonyMde <anthony.mamode@gmail.com>
2024-11-19 21:46:47 +0100

    Lock application in portrait mode.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:kaizen/domain/usecases/providers/check_users_logged_in_use_case_provider.dart';
 import 'package:kaizen/firebase_options.dart';
@@ -10,6 +11,10 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown
+  ]);
   final container = ProviderContainer();
   final hasAccount = await container.read(checkUserLoggedInUseCaseProvider).call();
 
