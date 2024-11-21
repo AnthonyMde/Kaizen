1738c264ac08c6895c7399ea0e96cf8436021b7d
AnthonyMde <anthony.mamode@gmail.com>
2024-11-21 18:25:26 +0100

    Cloud function updated to reset challeges status to isCompleted: false at 4am.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -17,21 +17,17 @@ import { onSchedule } from "firebase-functions/scheduler";
 admin.initializeApp();
 
 export const resetChallengesStatus = onSchedule(
-    { schedule: "*/2 * * * *", timeZone: "Europe/Paris" }, async () => {
+    { schedule: "0 4 * * *", timeZone: "Europe/Paris" }, async () => {
         const db = admin.firestore();
         const challengersSnapshot = await db.collection("challengers").get();
-        logger.info(`Found ${challengersSnapshot.size} challengers`);
 
         for (const challengerDoc of challengersSnapshot.docs) {
-            logger.info(`Processing challenger: ${challengerDoc.id}`);
             const challengesSnapshot = await challengerDoc.ref.collection("challenges").get();
-            logger.info(`Found ${challengesSnapshot.size} challenges for challenger ${challengerDoc.id}`);
+
             for (const challengeDoc of challengesSnapshot.docs) {
-                logger.info(`Updating challenge: ${challengeDoc.id}`);
                 await challengeDoc.ref.update({ isCompleted: false });
-                logger.info(`Challenge ${challengeDoc.id} updated with result`);
             }
         }
         
-        logger.info("V4 Challenges status updated");
+        logger.info("Challenges status reset to isCompleted: false");
     });
