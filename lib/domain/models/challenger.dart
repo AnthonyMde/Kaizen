3acc89ba82fdcb080e85e8cfd47e19de8c1513e6
Jean Vallon <jvallon44@gmail.com>
2024-11-24 23:31:06 +0100

    clean: challenger model

--- lib/domain/models/challenger.dart
+++ lib/domain/models/challenger.dart
@@ -5,11 +5,12 @@ part 'challenger.g.dart';
 
 @freezed
 class Challenger with _$Challenger {
-  const factory Challenger(
-      {required String id,
-      required String name,
-      required List<Challenge> challenges,
-      required int failures}) = _Challenger;
+  const factory Challenger({
+    required String id,
+    required String name,
+    required List<Challenge> challenges,
+    required int failures,
+  }) = _Challenger;
 
   factory Challenger.fromJson(Map<String, dynamic> json) =>
       _$ChallengerFromJson(json);
