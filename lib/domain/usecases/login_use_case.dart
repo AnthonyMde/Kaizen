29127ff6a61074e3df02b5ad834324af9a3cdbea
AnthonyMde <anthony.mamode@gmail.com>
2024-12-01 12:46:15 +0100

    Use real database values to login.

--- lib/domain/usecases/login_use_case.dart
+++ lib/domain/usecases/login_use_case.dart
@@ -1,30 +1,28 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:kaizen/data/repositories/providers/challenger_repository_provider.dart';
 import 'package:kaizen/domain/errors/login_errors.dart';
-import 'package:kaizen/domain/models/challenger.dart';
 
 import '../../data/repositories/providers/login_repository_provider.dart';
 
 class LoginUseCase {
-  final authorizedUsers = [
-    const Challenger(id: '1', name: 'Towny', challenges: [], isWasted: false),
-    const Challenger(id: '2', name: 'Clowi', challenges: [], isWasted: false),
-    const Challenger(id: '3', name: 'Jack', challenges: [], isWasted: false),
-  ];
   final Ref ref;
 
   LoginUseCase(this.ref);
 
-  Future<void> call(String name) {
+  Future<void> call(String name) async {
     if (name.trim().isEmpty) {
       throw LoginError.isNameEmpty();
     }
 
-    if (!authorizedUsers.map((user) => user.name).contains(name)) {
+    final challengers =
+        await ref.read(challengerRepositoryProvider).watchChallengers().first;
+
+    if (!challengers.map((challenger) => challenger.name).contains(name)) {
       throw LoginError.unauthorized();
     }
 
     return ref
         .read(accountRepositoryProvider)
-        .login(authorizedUsers.firstWhere((user) => user.name == name));
+        .login(challengers.firstWhere((user) => user.name == name));
   }
 }
